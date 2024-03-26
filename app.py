8233ebdad28b81ce81e91fcee306deceacab9e63
inderkiranbhamra <140848814+inderkiranbhamra@users.noreply.github.com>
2024-03-26 14:12:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,52 +10,44 @@ CORS(app)
 
 app.secret_key = 'inderkiran@24'
 
-# MySQL database configuration
-DB_HOST = '217.21.94.103'
-DB_NAME = 'u813060526_gameathonregis'
-DB_USER = 'u813060526_gameathonregis'
-DB_PASSWORD = '135@Hack'
 
-# Connect to the MySQL database
-conn = mysql.connector.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
-cursor = conn.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS UniqueIGNVALO (ign VARCHAR(255) UNIQUE)")
-conn.commit()
-# cursor.execute("DROP TABLE IF EXISTS UniqueIGN")
+# cursor.execute("CREATE TABLE IF NOT EXISTS UniqueIGNVALO (ign VARCHAR(255) UNIQUE)")
 # conn.commit()
-cursor.execute('''CREATE TABLE IF NOT EXISTS Valoregistrations (
-    team_name VARCHAR(255) PRIMARY KEY,
-    college_name VARCHAR(255),
-    leader_name VARCHAR(255),
-    leader_ign VARCHAR(255) UNIQUE,
-    leader_game_id VARCHAR(255) UNIQUE,
-    leader_id_no VARCHAR(255) UNIQUE,
-    leader_contact VARCHAR(255) UNIQUE,
-    leader_email VARCHAR(255) UNIQUE,
-    p2_name VARCHAR(255),
-    p2_ign VARCHAR(255) UNIQUE,
-    p2_game_id VARCHAR(255) UNIQUE,
-    p2_id_no VARCHAR(255) UNIQUE,
-    p2_contact VARCHAR(255) UNIQUE,
-    p3_name VARCHAR(255),
-    p3_ign VARCHAR(255) UNIQUE,
-    p3_game_id VARCHAR(255) UNIQUE,
-    p3_id_no VARCHAR(255) UNIQUE,
-    p3_contact VARCHAR(255) UNIQUE,
-    p4_name VARCHAR(255),
-    p4_ign VARCHAR(255) UNIQUE,
-    p4_game_id VARCHAR(255) UNIQUE,
-    p4_id_no VARCHAR(255) UNIQUE,
-    p4_contact VARCHAR(255) UNIQUE,
-    p5_name VARCHAR(255),
-    p5_ign VARCHAR(255) UNIQUE,
-    p5_game_id VARCHAR(255) UNIQUE,
-    p5_id_no VARCHAR(255) UNIQUE,
-    p5_contact VARCHAR(255) UNIQUE
-);
-''')
-conn.commit()
+# # cursor.execute("DROP TABLE IF EXISTS UniqueIGN")
+# # conn.commit()
+# cursor.execute('''CREATE TABLE IF NOT EXISTS Valoregistrations (
+#     team_name VARCHAR(255) PRIMARY KEY,
+#     college_name VARCHAR(255),
+#     leader_name VARCHAR(255),
+#     leader_ign VARCHAR(255) UNIQUE,
+#     leader_g ame_id VARCHAR(255) UNIQUE,
+#     leader_id_no VARCHAR(255) UNIQUE,
+#     leader_contact VARCHAR(255) UNIQUE,
+#     leader_email VARCHAR(255) UNIQUE,
+#     p2_name VARCHAR(255),
+#     p2_ign VARCHAR(255) UNIQUE,
+#     p2_game_id VARCHAR(255) UNIQUE,
+#     p2_id_no VARCHAR(255) UNIQUE,
+#     p2_contact VARCHAR(255) UNIQUE,
+#     p3_name VARCHAR(255),
+#     p3_ign VARCHAR(255) UNIQUE,
+#     p3_game_id VARCHAR(255) UNIQUE,
+#     p3_id_no VARCHAR(255) UNIQUE,
+#     p3_contact VARCHAR(255) UNIQUE,
+#     p4_name VARCHAR(255),
+#     p4_ign VARCHAR(255) UNIQUE,
+#     p4_game_id VARCHAR(255) UNIQUE,
+#     p4_id_no VARCHAR(255) UNIQUE,
+#     p4_contact VARCHAR(255) UNIQUE,
+#     p5_name VARCHAR(255),
+#     p5_ign VARCHAR(255) UNIQUE,
+#     p5_game_id VARCHAR(255) UNIQUE,
+#     p5_id_no VARCHAR(255) UNIQUE,
+#     p5_contact VARCHAR(255) UNIQUE
+# );
+# ''')
+# conn.commit()
 
 # Email configuration
 sender_email = 'hackoverflow@cumail.in'
@@ -114,6 +106,15 @@ def generate_auth_link(token, data):
 #     return False
 
 def check_duplicate_ign(data):
+    # MySQL database configuration
+    DB_HOST = '217.21.94.103'
+    DB_NAME = 'u813060526_valo'
+    DB_USER = 'u813060526_valo'
+    DB_PASSWORD = '135@Hack'
+
+    # Connect to the MySQL database
+    conn = mysql.connector.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
+    cursor = conn.cursor()
     ign_set = set()
     igns = [data['team_name'], data['leader_ign'], data['leader_game_id'], data['leader_id_no'], data['leader_contact'], data['leader_email'], data['p2_ign'], data['p2_game_id'], data['p2_id_no'], data['p2_contact'], data['p3_ign'], data['p3_game_id'], data['p3_id_no'], data['p3_contact'], data['p4_ign'], data['p4_game_id'], data['p4_id_no'], data['p4_contact'], data['p5_ign'], data['p5_game_id'], data['p5_id_no'], data['p5_contact']]
     field_names = ['Team Name', 'Leader IGN', 'Leader Game ID', 'Leader ID Number', 'Leader Contact', 'Leader Email', 'P2 IGN', 'P2 Game ID', 'P2 ID Number', 'P2 Contact', 'P3 IGN', 'P3 Game ID', 'P3 ID Number', 'P3 Contact', 'P4 IGN', 'P4 Game ID', 'P4 ID Number', 'P4 Contact', 'P5 IGN', 'P5 Game ID', 'P5 ID Number', 'P5 Contact']
@@ -156,7 +157,7 @@ def send_email():
         if duplicate_fields:
             return jsonify({'message': f'Duplicate data found at {duplicate_fields}: {duplicate_ign}.'}), 400
         else:
-            return jsonify({'message': 'Duplicate data detected.'}), 400
+            return jsonify({'message': f'Duplicate data found: {duplicate_ign}.'}), 400
 
 
     email = data['leader_email']
@@ -184,6 +185,15 @@ def send_email():
 
 @app.route('/verify/<token>', methods=['GET'])
 def verify(token):
+    # MySQL database configuration
+    DB_HOST = '217.21.94.103'
+    DB_NAME = 'u813060526_valo'
+    DB_USER = 'u813060526_valo'
+    DB_PASSWORD = '135@Hack'
+
+    # Connect to the MySQL database
+    conn = mysql.connector.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
+    cursor = conn.cursor()
     if token in email_tokens.values():
         emails = [key for key, value in email_tokens.items() if value == token][0]
         data = request.args.to_dict()
@@ -208,6 +218,7 @@ def verify(token):
         except mysql.connector.Error as err:
             print("Error inserting data:", err)
             conn.rollback()
-            return jsonify({'message': 'Error inserting data into database.'}), 500
+            error_message = f"Error inserting data into database: {err}"
+            return jsonify({'message': error_message}), 500
     else:
         return jsonify({'message': 'Invalid or expired verification link.'}), 400
